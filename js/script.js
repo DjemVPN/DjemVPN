fc4aec390577675fe775f4c79761ee3609a72968
Djemchik <jemil1.2004@mail.ru>
2024-03-18 22:23:06 +0300

    Update HolyShit

--- js/script.js
+++ js/script.js
@@ -7,12 +7,29 @@ const CallToHelp = document.querySelector('.supheader-right__flex')
 // const TransferToTariffs = document.querySelector('.add_more')
 // const PromoteFlex = document.querySelector('section-promotion')
 const BuyPoland = document.querySelector('.BuyPoland')
-const BuyNetherlands = document.querySelector('.supheader-right__flex')
-const BuyTurkey = document.querySelector('.supheader-right__flex')
+const BuyNetherlands = document.querySelector('.BuyNetherlands')
+const BuyTurkey = document.querySelector('.BuyTurkey')
+const BuyJapan = document.querySelector('.BuyJapan')
+const BuyMoscow = document.querySelector('.BuyMoscow')
+const BuyUkraine = document.querySelector('.BuyUkraine')
+
+
 
 CallToHelp.onclick = function(){
     window.location.href = "https://t.me/DJEMVPN_supporter"
 }
 BuyPoland.onclick = function(){
     window.location.href = "https://vpnkeys.ru/getkey/PolandServer"
+}
+BuyNetherlands.onclick = function(){
+    window.location.href = "https://vpnkeys.ru/getkey/DjemNL"
+}
+BuyTurkey.onclick = function(){
+    window.location.href = "https://vpnkeys.ru/getkey/Djem-Turkey"
+}
+BuyJapan.onclick = function(){
+    window.location.href = "https://vpnkeys.ru/getkey/djem-japan"
+}
+BuyMoscow.onclick = function(){
+    window.location.href = "https://vpnkeys.ru/getkey/djem-moscow"
 }
