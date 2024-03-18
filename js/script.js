c9200b0a96cf015e69ad1bb1e8885f4f3eaf57e7
Djemchik <jemil1.2004@mail.ru>
2024-03-18 21:36:33 +0300

    Update HolyShit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// const PromotionImage = document.querySelector('.section-promotion__image')
+// TarifsCard.onclick=function(){
+//     window.location.href = '../html';
+// };
+const CallToHelp = document.querySelector('.supheader-right__flex')
+// const TransferToTariffs = document.querySelector('.add_more')
+// const PromoteFlex = document.querySelector('section-promotion')
+const BuyPoland = document.querySelector('.BuyPoland')
+const BuyNetherlands = document.querySelector('.supheader-right__flex')
+const BuyTurkey = document.querySelector('.supheader-right__flex')
+
+CallToHelp.onclick = function(){
+    window.location.href = "https://t.me/DJEMVPN_supporter"
+}
+BuyPoland.onclick = function(){
+    window.location.href = "https://vpnkeys.ru/getkey/PolandServer"
+}
